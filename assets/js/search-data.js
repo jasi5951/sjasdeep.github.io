fa17538747e90335939a81595d156e354c0fe774
sjasdeep <89618452+sjasdeep@users.noreply.github.com>
2025-08-13 03:35:26 +0000

    Deploying to gh-pages from @ sjasdeep/sjasdeep.github.io@7afd21f558d601b70d7e01336ae0e548098790a5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -413,51 +413,26 @@ ninja.data = [{
           section: "News",},{id: "news-presented-at-the-undergraduate-research-symposium",
           title: 'Presented at the Undergraduate Research Symposium',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-image-compression",
+          title: 'Image Compression',
+          description: "A linear algebra project exploring image compression through Singular Value Decomposition (SVD) and Fourier Transform techniques.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-chladni-plates",
+          title: 'Chladni Plates',
+          description: "Modeling Chladni plate vibration patterns using PDEs to predict and compare spatial structures at natural frequencies.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-minigit",
+          title: 'MiniGit',
+          description: "A lightweight version control system mimicking Git’s core features—add, commit, checkout, and search—implemented without a remote server.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-infectious-disease-model-on-network",
+          title: 'Infectious Disease Model on Network',
+          description: "Simulating epidemic spread on contact networks using graph theory to compare precise face-to-face data with coarse but accessible co-presence data.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
