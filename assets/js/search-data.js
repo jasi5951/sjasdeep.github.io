7583023e7a1eb62cf6e2d4d0b5f95b71adea2d10
sjasdeep <89618452+sjasdeep@users.noreply.github.com>
2025-08-16 05:43:22 +0000

    Deploying to gh-pages from @ sjasdeep/sjasdeep.github.io@07146d1a88d393ca658be769e3bde9e9d23f8af8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/resume";
+            window.location.href = "/_pages/resume/";
           },
         },{id: "nav-cv",
           title: "cv",
